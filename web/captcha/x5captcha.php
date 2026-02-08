f2abcb56099cfdb9aef8d7aab7cde5c62390dacb
alessandro-satanassi <alessandro@cittini.it>
2026-02-08 23:28:30 +0100

    agg home

--- web/captcha/x5captcha.php
+++ web/captcha/x5captcha.php
@@ -1,7 +1,7 @@
 <?php
 include("../res/x5engine.php");
-$nameList = array("s4z","8sh","ue3","uxs","n2h","xzz","pzl","478","kau","xd7");
-$charList = array("M","7","J","W","V","H","2","R","L","X");
+$nameList = array("8jt","xwj","zhx","cym","nc7","g2t","c4j","kaw","2wy","nvx");
+$charList = array("W","P","H","5","5","P","F","S","J","T");
 $cpt = new X5Captcha($nameList, $charList);
 //Check Captcha
 if ($_GET["action"] == "check")
