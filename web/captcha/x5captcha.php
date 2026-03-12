c4761810c3bb2fce0e690a062d34f4aad44635e2
alessandro-satanassi <alessandro@cittini.it>
2026-03-12 23:33:08 +0100

    agg homepage

--- web/captcha/x5captcha.php
+++ web/captcha/x5captcha.php
@@ -1,7 +1,7 @@
 <?php
 include("../res/x5engine.php");
 $nameList = array("aya","myn","p3u","85s","ttt","j2h","as7","dsj","dxw","pjn");
-$charList = array("6","A","M","T","M","7","2","S","P","4");
+$charList = array("A","L","U","M","6","X","F","D","T","F");
 $cpt = new X5Captcha($nameList, $charList);
 //Check Captcha
 if ($_GET["action"] == "check")
--- web/res/x5settings.php
+++ web/res/x5settings.php
@@ -56,8 +56,8 @@ $imSettings['password_policy'] = array(
 |	Captcha
 |-------------------------------
 */ImTopic::$captcha_code = "		<div class=\"x5captcha-wrap\">
-			<label for=\"jvp4d02u-imCpt\">Parola di controllo:</label><br />
-			<input type=\"text\" id=\"jvp4d02u-imCpt\" class=\"imCpt\" name=\"imCpt\" maxlength=\"5\" />
+			<label for=\"o09ejwwr-imCpt\">Parola di controllo:</label><br />
+			<input type=\"text\" id=\"o09ejwwr-imCpt\" class=\"imCpt\" name=\"imCpt\" maxlength=\"5\" />
 		</div>
 ";
 
